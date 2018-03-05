e34366eba726e07061f3a0f56196078e255900a8
Dave Beck <dacb@uw.edu>
2018-03-05 15:06:54 -0800

    create environment for keras

---
+++ Wi18_content/DSMCER/slidegen/envcreate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+conda create -n keras
+source activate keras
+conda install -c conda-forge keras 
+conda install ipython
+conda install jupyter
+conda install pandas
+conda install scipy
+conda install seaborn
+conda install scikit-learn
